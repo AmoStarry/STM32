cc2aa1a56433ef13482214affdbf2b29abb84290
santigongming <120110090+santigongming@users.noreply.github.com>
2023-04-06 22:49:48 +0800

    LED闪烁

--- Day3/User/main.c
+++ Day3/User/main.c
@@ -1,17 +1,21 @@
 #include "stm32f10x.h" 
+#include "Delay.h"
 int main()
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	GPIO_InitTypeDef  GPIO_Initstructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_8;
 	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_50MHz;
 	
 	GPIO_Init(GPIOA,&GPIO_Initstructure);
 	//GPIO_SetBits(GPIOA,GPIO_Pin_8);
-	GPIO_ResetBits(GPIOA,GPIO_Pin_8);
+	//GPIO_ResetBits(GPIOA,GPIO_Pin_8);
 	while(1)
 	{
-		
+		GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_RESET);
+		Delay_ms(100);
+		GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_SET);
+		Delay_ms(100);
 	}
 }
