8fcc5c7a303618ce3fd9d3385831e10587ff1ada
santigongming <120110090+santigongming@users.noreply.github.com>
2023-04-11 14:03:19 +0800

    按键

---
+++ Key/User/main.c
@@ -0,0 +1,20 @@
+#include "stm32f10x.h"
+#include "Delay.h"
+#include "LED.h" 
+int main()
+{
+	LED_Init();
+	while(1)
+	{
+		LED1_ON();
+		Delay_ms(500);
+		LED1_OFF();
+		Delay_ms(500);
+		LED2_ON();
+		Delay_ms(500);
+		LED2_OFF();
+		Delay_ms(500);
+		
+		
+	}
+}
--- buzzer/User/main.c
+++ buzzer/User/main.c
@@ -1,8 +1,8 @@
 #include "stm32f10x.h" 
 int main()
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	GPIO_InitTypeDef  GPIO_Initstructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_8;
 	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_50MHz;
