ebf669c9d1196b2a4fbb60127b579861597cbb8e
santigongming <120110090+santigongming@users.noreply.github.com>
2023-06-04 22:59:53 +0800

    DMA_ADC

--- STM32_BZ/DMA_ADC/User/main.c
+++ STM32_BZ/DMA_ADC/User/main.c
@@ -3,24 +3,25 @@
 #include "OLED.h"
 #include "AD.h"
 
-uint16_t ADValue;
-float Voltage;
-int main()
+int main(void)
 {
 	
 	OLED_Init();
 	AD_Init();
 	
-	OLED_ShowString(1,1,"ADValue");
+	OLED_ShowString(1,1,"AD0");
+     OLED_ShowString(2,1,"AD1");
+     OLED_ShowString(3,1,"AD2");
+     OLED_ShowString(4,1,"AD3");
 	
 	while(1)
 	{
-          ADValue = AD_GetValue();
-          Voltage = (float)ADValue /4095*3.3;
           
-		OLED_ShowNum(1,9,ADValue,4);
-          OLED_ShowNum(2,9,Voltage,1);
-          OLED_ShowNum(2,11,(uint16_t)(Voltage*100)%100,2);
-		Delay_ms(100);
+		OLED_ShowNum(1,5,AD_Value[0],4);
+          OLED_ShowNum(2,5,AD_Value[1],4);
+          OLED_ShowNum(3,5,AD_Value[2],4);
+          OLED_ShowNum(4,5,AD_Value[3],4);
+                       
+		Delay_ms(1000);
 	}
 }
