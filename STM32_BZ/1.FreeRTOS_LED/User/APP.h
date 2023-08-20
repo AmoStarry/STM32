9f43589abb5c132044de80c668eea285694830d8
santigongming <120110090+santigongming@users.noreply.github.com>
2023-08-20 19:27:21 +0800

    RTOS跑多任务，验证单任务没问题，回了一点软件仿真

---
+++ STM32_BZ/1.FreeRTOS_LED/User/APP.h
@@ -0,0 +1,5 @@
+#ifndef __APP_H
+#define __APP_H
+
+void APP_turn(void);
+#endif
--- STM32_BZ/1.FreeRTOS_LED/User/main.c
+++ STM32_BZ/1.FreeRTOS_LED/User/main.c
@@ -1,24 +1,22 @@
 #include "stm32f10x.h"
 #include "FreeRTOS.h"
+#include "task.h"
 #include "Delay.h" 
+#include "LED.h"
+#include "APP.h"
 int main()
 {	
-	GPIO_InitTypeDef  GPIO_Initstructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_8;
-	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC,&GPIO_Initstructure);
+     LED_Init();
+     
 	while(1)
 	{
-		GPIO_ResetBits(GPIOA,GPIO_Pin_8);
-		Delay_ms(500);
-		GPIO_SetBits(GPIOA,GPIO_Pin_8);
-		Delay_ms(500);
-		/*方法二：
-		GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_RESET);//((BitAction)0)强制类型转换
-		Delay_ms(500);
-		GPIO_WriteBit(GPIOA,GPIO_Pin_8,Bit_SET);//((BitAction)1)
-		Delay_ms(500);*/
+          LED1_ON();
+          LED2_ON();
+          Delay_ms(100);
+          LED1_OFF();
+          LED2_OFF();
+          Delay_ms(100);
+          
+        //APP_turn();  
 	}
 }
