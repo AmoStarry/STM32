fb296b5d8245cfcc0fb2483accda48ccd0027ddb
santigongming <120110090+santigongming@users.noreply.github.com>
2023-08-22 14:56:53 +0800

    解决了多任务不跑的问题，重点重点：任务里面不能跑空占用资源，一定得干点事

--- STM32_BZ/1.FreeRTOS_LED/User/APP.c
+++ STM32_BZ/1.FreeRTOS_LED/User/APP.c
@@ -11,7 +11,7 @@
   * @retval 无
   */
 #define START_TASK_PRIO         1
-#define START_TASK_STACK_SIZE   128
+#define START_TASK_STACK_SIZE   512
 TaskHandle_t    start_task_handler;
 void start_task( void * pvParameters );
 
@@ -23,7 +23,7 @@ void start_task( void * pvParameters );
   * @retval 无
   */
 #define TASK1_PRIO        2
-#define TASK1_STACK_SIZE  128
+#define TASK1_STACK_SIZE  50
 TaskHandle_t      task1_handler;
 void task1( void *pvParameters);
 
@@ -35,7 +35,7 @@ void task1( void *pvParameters);
   * @retval 无
   */
 #define TASK2_PRIO        3
-#define TASK2_STACK_SIZE  128
+#define TASK2_STACK_SIZE  50
 TaskHandle_t      task2_handler;
 void task2( void *pvParameters);
 
@@ -47,7 +47,7 @@ void task2( void *pvParameters);
   * @retval 无
   */
 #define TASK3_PRIO        4
-#define TASK3_STACK_SIZE  128
+#define TASK3_STACK_SIZE  50
 TaskHandle_t      task3_handler;
 void task3( void *pvParameters);
 
@@ -61,6 +61,21 @@ void APP_turn(void)
                 (void *                 )   NULL,
                 (UBaseType_t            )   START_TASK_PRIO,
                 (TaskHandle_t *         )   &start_task_handler );
+                
+//         xTaskCreate((TaskFunction_t         )   task1,
+//                     (char *                 )   "task1",
+//                     (configSTACK_DEPTH_TYPE )   TASK1_STACK_SIZE,
+//                     (void *                 )   NULL,
+//                     (UBaseType_t            )   TASK1_PRIO,
+//                     (TaskHandle_t *         )   &task1_handler );
+//                     
+//         xTaskCreate((TaskFunction_t         )   task2,
+//                     (char *                 )   "task2",
+//                     (configSTACK_DEPTH_TYPE )   TASK2_STACK_SIZE,
+//                     (void *                 )   NULL,
+//                     (UBaseType_t            )   TASK2_PRIO,
+//                     (TaskHandle_t *         )   &task2_handler );
+                     
     vTaskStartScheduler();  
 }
 
@@ -68,80 +83,86 @@ void start_task( void * pvParameters )
 {
     while(1)
     {
-        //printf("task1正在运行！！！\r\n");
-        LED1_Turn();
-        vTaskDelay(200);
-    }
-//    taskENTER_CRITICAL();               /* 进入临界区 */
-//    xTaskCreate((TaskFunction_t         )   task1,
-//                (char *                 )   "task1",
-//                (configSTACK_DEPTH_TYPE )   TASK1_STACK_SIZE,
-//                (void *                 )   NULL,
-//                (UBaseType_t            )   TASK1_PRIO,
-//                (TaskHandle_t *         )   &task1_handler );
-//                
-//    xTaskCreate((TaskFunction_t         )   task2,
-//                (char *                 )   "task2",
-//                (configSTACK_DEPTH_TYPE )   TASK2_STACK_SIZE,
-//                (void *                 )   NULL,
-//                (UBaseType_t            )   TASK2_PRIO,
-//                (TaskHandle_t *         )   &task2_handler );
-//                
-//    xTaskCreate((TaskFunction_t         )   task3,
-//                (char *                 )   "task3",
-//                (configSTACK_DEPTH_TYPE )   TASK3_STACK_SIZE,
-//                (void *                 )   NULL,
-//                (UBaseType_t            )   TASK3_PRIO,
-//                (TaskHandle_t *         )   &task3_handler );
-//                
-//    vTaskDelete(NULL);
-//    taskEXIT_CRITICAL();                /* 退出临界区 */
+//         LED1_ON();
+//         vTaskDelay(500);
+//         LED1_OFF();
+//         vTaskDelay(500);
+         taskENTER_CRITICAL();               /* 进入临界区 */
+         xTaskCreate((TaskFunction_t         )   task1,
+                     (char *                 )   "task1",
+                     (configSTACK_DEPTH_TYPE )   TASK1_STACK_SIZE,
+                     (void *                 )   NULL,
+                     (UBaseType_t            )   TASK1_PRIO,
+                     (TaskHandle_t *         )   &task1_handler );
+                     
+         xTaskCreate((TaskFunction_t         )   task2,
+                     (char *                 )   "task2",
+                     (configSTACK_DEPTH_TYPE )   TASK2_STACK_SIZE,
+                     (void *                 )   NULL,
+                     (UBaseType_t            )   TASK2_PRIO,
+                     (TaskHandle_t *         )   &task2_handler );
+                     
+         xTaskCreate((TaskFunction_t         )   task3,
+                     (char *                 )   "task3",
+                     (configSTACK_DEPTH_TYPE )   TASK3_STACK_SIZE,
+                     (void *                 )   NULL,
+                     (UBaseType_t            )   TASK3_PRIO,
+                     (TaskHandle_t *         )   &task3_handler );
+                     
+         vTaskDelete(NULL);
+         taskEXIT_CRITICAL();                /* 退出临界区 */
+     }
 }
 
-///* 任务一，实现LED1每500ms翻转一次 */
-//void task1( void * pvParameters )
-//{
-//    while(1)
-//    {
-//        //printf("task1正在运行！！！\r\n");
-//        LED1_ON();
-//        vTaskDelay(500);
-//        LED1_OFF();
-//        vTaskDelay(500);
-//    }
-//}
+/* 任务一，实现LED1每500ms翻转一次 */
+void task1( void * pvParameters )
+{
+    while(1)
+    {
+        //printf("task1正在运行！！！\r\n");
+        LED1_ON();
+        vTaskDelay(500);
+        LED1_OFF();
+        vTaskDelay(500);
+    }
+}
 
-///* 任务二，实现LED2每500ms翻转一次 */
-//void task2( void * pvParameters )
-//{
-//    while(1)
-//    {
-//        //printf("task2正在运行！！！\r\n");
-//        LED2_ON();
-//        vTaskDelay(500);
-//        LED2_OFF();
-//        vTaskDelay(500);
-//    }
-//}
+/* 任务二，实现LED2每500ms翻转一次 */
+void task2( void * pvParameters )
+{
+    while(1)
+    {
+        //printf("task2正在运行！！！\r\n");
+        LED2_ON();
+        vTaskDelay(500);
+        LED2_OFF();
+        vTaskDelay(500);
+    }
+}
 
-///* 任务三 */
-//void task3( void * pvParameters )
-//{
-////    uint8_t key = 0;
-//    while(1)
-//    {
-////        printf("task3正在运行！！！\r\n");
-////        key = key_scan(0);
-////        if(key == KEY0_PRES)
-////        {
-////            if(task1_handler != NULL)
-////            {
-////                printf("删除task1任务\r\n");
-////                vTaskDelete(task1_handler);
-////                task1_handler = NULL;
-////            }
+/* 任务三 */
+void task3( void * pvParameters )
+{
+//    uint8_t key = 0;
+    while(1)
+    {
+        LED2_ON();
+        vTaskDelay(500);
+        LED2_OFF();
+        vTaskDelay(500);
+         
+//        printf("task3正在运行！！！\r\n");
+//        key = key_scan(0);
+//        if(key == KEY0_PRES)
+//        {
+//            if(task1_handler != NULL)
+//            {
+//                printf("删除task1任务\r\n");
+//                vTaskDelete(task1_handler);
+//                task1_handler = NULL;
+//            }
 
-////        }
-////        vTaskDelay(10);
-//    }
-//}
+//        }
+//        vTaskDelay(10);
+    }
+}
--- STM32_BZ/1.FreeRTOS_LED/User/APP.h
+++ STM32_BZ/1.FreeRTOS_LED/User/APP.h
@@ -2,4 +2,8 @@
 #define __APP_H
 
 void APP_turn(void);
+void start_task( void * pvParameters );
+void task1( void *pvParameters);
+void task2( void *pvParameters);
+void task3( void *pvParameters);
 #endif
